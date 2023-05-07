122820e7080e51c93464513603b6121415af283f
sailing-innocent <sailing-innocent@foxmail.com>
2023-05-07 11:50:21 +0800

    add basis-v0.1.0 document

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,13 @@
+-- set the project name
 set_project("graphics-api-sample")
 
+-- add debug and release modes
 add_rules("mode.debug", "mode.release")
 
+-- set our target "hello.exe"
 target("hello")
+    -- add our source files 
     add_files("src/hello.cpp")
 target_end()
+
+-- that's all~
